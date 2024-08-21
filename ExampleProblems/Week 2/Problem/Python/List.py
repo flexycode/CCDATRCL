b9d5967b6ef9c3d73d878ec06639353e9882c970
Jay <flexyledger@gmail.com>
2024-08-22 01:33:44 +0800

    🧪 Create List.py
    Add List.py

---
+++ ExampleProblems/Week 2/Problem/Python/List.py
@@ -0,0 +1,20 @@
+# Example program that demonstrates the use of a List
+def list_example():
+    # Create a List of strings
+    kpop = ["TripleS", "IU", "Twice", "Exid"]
+
+    # Print the list elements
+    print("List elements:")
+    print(kpop)
+
+    # Add an element to the list
+    kpop.append("BlackPink")
+    print("Updated list elements:")
+    print(kpop)
+
+    # Remove an element from the list
+    kpop.remove("IU")
+    print("Updated list elements:")
+    print(kpop)
+
+list_example()
