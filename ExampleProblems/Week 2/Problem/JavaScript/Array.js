bc138d8992e28e741784863cacc0cd947eba5807
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-08-22 01:17:28 +0800

    🧪 Create Array.js

---
+++ ExampleProblems/Week 2/Problem/JavaScript/Array.js
@@ -0,0 +1,20 @@
+// Example program that demonstrates the use of an array
+const arrayExample = () => {
+  // Declare and initialize an array of integers
+  let scores = [90, 80, 70, 95, 85];
+
+  // Print the array elements
+  console.log("Array elements:");
+  console.log(scores);
+
+  // Access an array element
+  console.log("Accessing an array element:");
+  console.log(scores[0]);
+
+  // Update an array element
+  scores[0] = 100;
+  console.log("Updated array elements:");
+  console.log(scores);
+};
+
+arrayExample();
