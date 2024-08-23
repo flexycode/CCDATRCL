a654de6fe1be5ca49fe6dd612a5b16d9f90fdca8
Jay Arre P. Talosig <35078433+flexycode@users.noreply.github.com>
2024-08-23 22:34:34 +0800

    ✨ Refactor List.js
    - Refactor my entire code

--- ProblemSet/Week 2/Problem/JavaScript/List.js
+++ ProblemSet/Week 2/Problem/JavaScript/List.js
@@ -1,21 +1,47 @@
-// Example program that demonstrates the use of a List (using Array)
-const listExample = () => {
-  // Create a List of strings
-  let colors = ["Red", "Green", "Blue", "Yellow"];
+let kpop = [];
 
-  // Print the list elements
-  console.log("List elements:");
-  console.log(colors);
+// Ask the user to enter the number of K-POP groups to add to the list
+let numGroups = parseInt(prompt("Enter the number of K-POP groups to add to the list: "));
 
-  // Add an element to the list
-  colors.push("Purple");
-  console.log("Updated list elements:");
-  console.log(colors);
+// Ask the user to enter the K-POP groups to add to the list
+for (let i = 0; i < numGroups; i++) {
+  let group = prompt(`Group ${i + 1}: `);
+  kpop.push(group);
+}
 
-  // Remove an element from the list
-  colors.splice(colors.indexOf("Green"), 1);
-  console.log("Updated list elements:");
-  console.log(colors);
-};
+// Print the list elements
+console.log("List KPOP:");
+console.log(kpop.join(" "));
 
-listExample();
+// Ask the user if they want to remove a K-POP group from the list
+let input = prompt("Do you want to remove a K-POP group from the list? (yes/no): ");
+
+if (input.toLowerCase() === "yes") {
+  // Ask the user to enter the K-POP group to remove
+  let removeGroup = prompt("Enter the K-POP group to remove: ");
+  let index = kpop.indexOf(removeGroup);
+  if (index !== -1) {
+    kpop.splice(index, 1);
+  }
+
+  // Print the updated list elements
+  console.log("Updated list KPOP:");
+  console.log(kpop.join(" "));
+} else {
+  console.log("No K-POP group removed from the list.");
+}
+
+// Ask the user if they want to add another K-POP group to the list
+input = prompt("Do you want to add another K-POP group to the list? (yes/no): ");
+
+if (input.toLowerCase() === "yes") {
+  // Ask the user to enter the new K-POP group
+  let newGroup = prompt("Enter the new K-POP group: ");
+  kpop.push(newGroup);
+
+  // Print the updated list elements
+  console.log("Updated list KPOP:");
+  console.log(kpop.join(" "));
+} else {
+  console.log("No new K-POP group added to the list.");
+}
