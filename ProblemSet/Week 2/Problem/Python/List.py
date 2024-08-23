7b612f5028143805ecde13f2f4b4ce6f66c6b0d7
Jay <flexyledger@gmail.com>
2024-08-23 23:00:20 +0800

    ✨ Refactor List.py
    - Refactor entire code base

--- ProblemSet/Week 2/Problem/Python/List.py
+++ ProblemSet/Week 2/Problem/Python/List.py
@@ -1,20 +1,42 @@
-# Example program that demonstrates the use of a List
-def list_example():
-    # Create a List of strings
-    kpop = ["TripleS", "IU", "Twice", "Exid"]
+kpop = []
 
-    # Print the list elements
-    print("List elements:")
-    print(kpop)
+# Ask the user to enter the number of K-POP groups to add to the list
+num_groups = int(input("Enter the number of K-POP groups to add to the list: "))
 
-    # Add an element to the list
-    kpop.append("BlackPink")
-    print("Updated list elements:")
-    print(kpop)
+# Ask the user to enter the K-POP groups to add to the list
+for i in range(num_groups):
+    group = input(f"Group {i + 1}: ")
+    kpop.append(group)
 
-    # Remove an element from the list
-    kpop.remove("IU")
-    print("Updated list elements:")
-    print(kpop)
+# Print the list elements
+print("List KPOP:")
+print(" ".join(kpop))
 
-list_example()
+# Ask the user if they want to remove a K-POP group from the list
+input_str = input("Do you want to remove a K-POP group from the list? (yes/no): ")
+
+if input_str.lower() == "yes":
+    # Ask the user to enter the K-POP group to remove
+    remove_group = input("Enter the K-POP group to remove: ")
+    if remove_group in kpop:
+        kpop.remove(remove_group)
+
+    # Print the updated list elements
+    print("Updated list KPOP:")
+    print(" ".join(kpop))
+else:
+    print("No K-POP group removed from the list.")
+
+# Ask the user if they want to add another K-POP group to the list
+input_str = input("Do you want to add another K-POP group to the list? (yes/no): ")
+
+if input_str.lower() == "yes":
+    # Ask the user to enter the new K-POP group
+    new_group = input("Enter the new K-POP group: ")
+    kpop.append(new_group)
+
+    # Print the updated list elements
+    print("Updated list KPOP:")
+    print(" ".join(kpop))
+else:
+    print("No new K-POP group added to the list.")
